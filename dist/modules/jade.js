6873d03ab7a81eaf3773b72fdc7f1a387b3186e5
Christian de Botton <cdebotton@gmail.com>
2015-01-11 09:55:41 -0500

    Adding jade support.

--- dist/modules/jade.js
+++ dist/modules/jade.js
@@ -6,6 +6,16 @@ var _interopRequire = function (obj) {
 
 var pruno = _interopRequire(require(".."));
 
+var path = _interopRequire(require("path"));
+
+var fs = _interopRequire(require("fs"));
+
+var loadPlugins = _interopRequire(require("gulp-load-plugins"));
+
+var distillOptions = _interopRequire(require("../utils/distillOptions"));
+
+var plugins = loadPlugins();
+
 var JadeTask = function JadeTask() {
   var params = arguments[0] === undefined ? {} : arguments[0];
   this.params = params;
@@ -14,12 +24,15 @@ var JadeTask = function JadeTask() {
 JadeTask.getDefaults = function () {
   return {
     entry: "::src/templates/**/*.jade",
-    dist: "::output"
+    dist: "::dist",
+    search: ["::src/templates/**/*.jade"]
   };
 };
 
 JadeTask.prototype.enqueue = function (gulp) {
   var params = arguments[1] === undefined ? {} : arguments[1];
+  var opts = distillOptions(JadeTask, params);
+
   gulp.src(params.entry).pipe(plugins.jade(opts)).pipe(gulp.dest(params.dist));
 };
 
--- src/modules/jade.js
+++ src/modules/jade.js
@@ -1,10 +1,19 @@
 import pruno from '..';
+import path from 'path';
+import fs from 'fs';
+import loadPlugins from 'gulp-load-plugins';
+import distillOptions from '../utils/distillOptions';
+
+var plugins = loadPlugins();
 
 class JadeTask {
   static getDefaults() {
     return {
       entry: '::src/templates/**/*.jade',
-      dist: '::output'
+      dist: '::dist',
+      search: [
+        '::src/templates/**/*.jade'
+      ]
     };
   }
 
@@ -13,6 +22,8 @@ class JadeTask {
   }
 
   enqueue(gulp, params = {}) {
+    var opts = distillOptions(JadeTask, params);
+
     gulp.src(params.entry)
       .pipe(plugins.jade(opts))
       .pipe(gulp.dest(params.dist));
